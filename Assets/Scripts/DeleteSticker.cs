a476d020287fb7a328250aa0849bf6fd627f2d50
Zach W <winegardner.55@osu.edu>
2018-03-24 15:56:45 -0400

    working simple mech

---
+++ Assets/Scripts/DeleteSticker.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteSticker : MonoBehaviour {
+
+	bool isOverTrash;  
+	float timer; 
+	float timerTime; 
+	float stateTimer = 0f; 
+
+	Vector3 originScale; 
+	Vector3 deletedScale; 
+
+	public bool _readyToDelete; 
+
+	SpriteRenderer spriteRenderer; 
+	public Color32 deletedColor; 
+	public Color32 originColor; 
+
+	void Start(){
+		spriteRenderer = GetComponent<SpriteRenderer> (); 
+		originColor = spriteRenderer.color; 
+		_readyToDelete = false; 
+		isOverTrash = false; 
+		timerTime = 0.33f;  
+		timer = timerTime; 
+
+		originScale = transform.localScale; 
+		deletedScale = new Vector3 (0.6f, 0.6f, 0.6f); 
+	}
+
+	void Update(){
+
+		if (isOverTrash) {
+			if (timer >=0)
+			timer -= Time.deltaTime; 
+		}
+
+		if (timer <= 0) {
+			_readyToDelete = true; 
+			stateTimer += Time.deltaTime*5; 
+			spriteRenderer.color = Color32.Lerp (originColor, deletedColor, stateTimer); 
+			Vector3 newScale; 
+			newScale = Vector3.Lerp (originScale, deletedScale, stateTimer); 
+			transform.localScale = newScale; 
+			//change color and size
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "trashcan") {
+			timer = timerTime; 
+			isOverTrash = true; 
+
+		}
+	}
+
+	void OnTriggerExit (Collider other){
+		isOverTrash = false; 
+		_readyToDelete = false; 
+		spriteRenderer.color = originColor; 
+		transform.localScale = originScale; 
+		timer = timerTime; 
+		stateTimer = 0f; 
+
+	}
+
+}
--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -1,98 +1,146 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class TouchInput : MonoBehaviour {
 
-	public LayerMask touchInputMask; 
 
-	private List<GameObject> touchlist = new List<GameObject> (); 
-	private GameObject[] touchesOld; 
-	private RaycastHit hit; 
+	public Text text; 
+	public float raylength;
+	public LayerMask stickerMask; 
+	public LayerMask inPlayMask; 
+	bool parented = false; 
+	GameObject recipientChild; 
+	GameObject sticker; 
+	public Transform touchParent; 
+	public float scaleSensitivity = 1f; 
+	public float minScale; 
+	public float maxScale; 
+	float startDist; 
+	float currentDist; 
+	float diffDist;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
-//		#if UNITY_EDITOR
-//
-//		if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0)) {
-//
-//			touchesOld = new GameObject[touchlist.Count]; 
-//			touchlist.CopyTo (touchesOld); 
-//			touchlist.Clear (); 
-//
-//			Ray ray = GetComponent<Camera> ().ScreenPointToRay (Input.mousePosition); 
-//				RaycastHit hit; 
-//
-//				if (Physics.Raycast (ray, out hit, touchInputMask)) {
-//
-//					GameObject recipient = hit.transform.gameObject; 
-//					touchlist.Add (recipient); 
-//
-//				if ( Input.GetMouseButtonDown(0)) {
-//						recipient.SendMessage ("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver); 
-//					}
-//
-//					if (Input.GetMouseButtonUp(0)) {
-//						recipient.SendMessage ("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver); 
-//					}
-//
-//				if (Input.GetMouseButton(0)) {
-//						recipient.SendMessage ("OnTouchMoving", hit.point, SendMessageOptions.DontRequireReceiver); 
-//					}
-//							
-//				}
-//	
-//		}
-//		#endif
 
-		if (Input.touchCount > 0) {
+		//Whenever there is a touch
+		if (Input.touchCount == 1) {
 
-//			touchesOld = new GameObject[touchlist.Count]; 
-//			touchlist.CopyTo (touchesOld); 
-//			touchlist.Clear (); 
 
-			foreach (Touch touch in Input.touches) {
-				//print (touch.fingerId); 
+			//store the world pos of the touch(es)
+			Vector3 touchPosZero = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);; 
+			//Vector3 touchPosOne = Camera.main.ScreenToWorldPoint (Input.GetTouch (1).position);; 
+			touchParent.position = touchPosZero;
+			
 
-				Ray ray = GetComponent<Camera> ().ScreenPointToRay (touch.position); 
 
-				if (Physics.Raycast (ray, out hit, touchInputMask)) {
+			//Check if that touch just began
+			if (Input.GetTouch(0).phase == TouchPhase.Began){
+				//Cast a ray
+				RaycastHit hit; 
+				Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
 
-					GameObject recipient = hit.transform.gameObject; 
-					print (recipient.name.ToString ()); 
+				//parent the empty object to the touch
 
-					if (touch.phase == TouchPhase.Began) {
-						recipient.SendMessage ("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
-						print ("parented object"); 
-						//parent the recipient to the touch
-					}
+				//If you hit a sticker UI object, parent a clone object to the touchpos
+				if (Physics.Raycast (ray, out hit, raylength, stickerMask)) {
+					sticker = hit.transform.gameObject; 
+					recipientChild = Instantiate (sticker, sticker.transform.position, Quaternion.identity); 
+					recipientChild.layer = 9; 
+					//parented = true; 
+					ParentObject(recipientChild, touchParent);   
 
-					if (touch.phase == TouchPhase.Ended) {
-						recipient.SendMessage ("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver); 
-						//unparent the child
-					}
-
-					if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
-						recipient.SendMessage ("OnTouchMoving", hit.point, SendMessageOptions.DontRequireReceiver); 
-						//set the childs position to the parent touch
-					}
-
-					if (touch.phase == TouchPhase.Canceled) {
-						recipient.SendMessage ("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver); 
-						//unparent
+				} else {
+					
+					//if its already in play just move it
+					if(Physics.Raycast(ray, out hit, raylength, inPlayMask)){
+						recipientChild = hit.transform.gameObject; 
+						//parented = true; 
+						ParentObject(recipientChild, touchParent);   
 					}
 				}
 			}
 
-//			foreach (GameObject g in touchesOld) {
-//				if (!touchlist.Contains (g)) {
-//					g.SendMessage ("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver); 
-//				}
+			// Ended or error, unparent
+			if (Input.GetTouch (0).phase == TouchPhase.Ended || Input.GetTouch (0).phase == TouchPhase.Canceled) {
+				//parented = false; 
+				UnParentObject (recipientChild); 
+				DeleteSticker DS = recipientChild.GetComponent<DeleteSticker> (); 
+				if (DS._readyToDelete) {
+					Destroy (recipientChild); 
+				}
 			}
+
+			//Set the cloned object touched to the touchpos
+//			if (parented) {
+//				Vector3 newpos = new Vector3 (touchPosZero.x, touchPosZero.y, recipientChild.transform.position.z); 
+//				recipientChild.transform.position = newpos;
+//
+//
+//			}
+		}
+
+		//For Zooming and Rotation
+		if (Input.touchCount == 2) {
+
+			//store the touch positions
+			Vector3 touchPosZero = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);; 
+			Vector3 touchPosOne = Camera.main.ScreenToWorldPoint (Input.GetTouch (1).position);; 
+			//touchParent.position = Vector3.Lerp (touchPosZero, touchPosOne, 0.5f);
+			//Set the touchparent to the first touch
+
+			// unparent the sticker if a finger was lifted
+			if (Input.GetTouch (0).phase == TouchPhase.Ended || Input.GetTouch (1).phase == TouchPhase.Ended) {
+				if (recipientChild != null && recipientChild.transform.parent != null) {
+					UnParentObject (recipientChild); 
+				}
+			}
+
+			//if there is a sticker that was touched 
+			if (recipientChild != null && recipientChild.transform.parent != null) {
+				
+				// Store both touches.
+				Touch touchZero = Input.GetTouch(0);
+				Touch touchOne = Input.GetTouch(1);
+
+				// Find the position in the previous frame of each touch.
+				Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+				Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+				// Find the magnitude of the vector (the distance) between the touches in each frame.
+				float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+				float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+				// Find the difference in the distances between each frame.
+				float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+				//print (deltaMagnitudeDiff.ToString ()); 
+
+				float xScale = recipientChild.transform.localScale.x; 
+				float yScale = recipientChild.transform.localScale.y; 
+
+				xScale -= deltaMagnitudeDiff * scaleSensitivity; 
+				xScale = Mathf.Clamp (xScale, minScale, maxScale); 
+				yScale = xScale;
+				Vector3 pinchedScale = new Vector3 (xScale, yScale, 1); 
+				recipientChild.transform.localScale = pinchedScale;  
+
+			}
+
 		}
 	}
+
+	void ParentObject(GameObject child, Transform parent){
+
+		child.transform.parent = parent; 
+	}
+
+	void UnParentObject(GameObject child){
+
+		if (recipientChild != null) {
+			
+			child.transform.parent = null; 
+		}
+	}
+
+
+}

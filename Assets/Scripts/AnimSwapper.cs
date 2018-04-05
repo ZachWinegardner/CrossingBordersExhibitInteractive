bbe4672006e37a946a6531c0ce54d2380e719965
Zach W <winegardner.55@osu.edu>
2018-04-04 23:08:55 -0400

    added animations

---
+++ Assets/Scripts/AnimSwapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimSwapper : MonoBehaviour {
+
+	public GameObject[] anims; 
+
+	void Start (){
+		foreach (GameObject anim in anims) {
+			anim.SetActive (false); 
+		}
+	}
+
+	public void SetAnim (int index){
+
+		foreach (GameObject anim in anims) {
+			anim.SetActive (false); 
+		}
+
+		anims [index - 1].SetActive (true); 
+	}
+}

18b23574cd4eac35f2a6452960061b85965b05bf
Zach Winegardner <winegardner.55@osu.edu>
2018-04-10 23:32:49 -0400

    Data reveal and attract

---
+++ Assets/Scripts/AttractAnimator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractAnimator : MonoBehaviour {
+
+	Vector3 startPos; 
+	public float offset; 
+	public float amp; 
+	public bool zoom; 
+	public float zValue; 
+
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position; 
+		zValue = transform.position.z; 
+
+	}
+
+	IEnumerator Zoom (){
+		zoom = true; 
+		yield return new WaitForSeconds (2); 
+		gameObject.SetActive (false); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (zoom) {
+			zValue += .2f; 
+			transform.position = startPos + new Vector3 (0f, 0f, -zValue);  
+		} else {
+			zValue = startPos.z; 
+			transform.position = startPos + new Vector3 (0f, (Mathf.Sin (Time.time + offset)) * amp, 0f);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/RevealData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealData : MonoBehaviour {
+
+	public int codeNumber = 0; 
+
+	public void CheckCode(int addition){
+		//add the amount from the buttons 1 or 10
+		codeNumber += addition; 
+		if (codeNumber == 33) {
+			Reveal (); 
+		}
+		//unless it equals 33, countdown
+		else
+			StartCoroutine (Countdown ()); 
+	}
+
+	void Reveal(){
+		transform.Translate (0, -1680, 0); 
+	}
+
+	public void Hide(){
+		transform.Translate (0, 1680, 0); 
+	}
+
+	public void Clear(){
+		codeNumber = 0; 
+	}
+
+	IEnumerator Countdown(){
+		//if it isn't 33 after 5 secs, reset it
+		yield return new WaitForSeconds (5); 
+		if (codeNumber != 33) {
+			Clear (); 
+		}
+	}
+}
